a7eee394e6bb8c41c6d68b3bea01f2d4837f5fa5
amerriman <amm434@gmail.com>
2016-02-06 14:54:11 -0700

    updated functionality

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
 
   # Example resource route with options:
   #   resources :products do
